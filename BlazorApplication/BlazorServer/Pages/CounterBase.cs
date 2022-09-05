37e2c9215d71cd38ea509a9b712c624d682090e8
Mazhar Jan <mazharj4565@gmail.com>
2022-09-05 11:13:43 +0500

    Split razor components into separate files

---
+++ BlazorApplication/BlazorServer/Pages/CounterBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Pages
+{
+    public class CounterBase:ComponentBase
+
+    {
+        protected int currentCount = 0;
+        protected string fontFamily = "Verdana";
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
